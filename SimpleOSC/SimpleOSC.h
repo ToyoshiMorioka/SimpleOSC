130cfb915e047de5ca42f714afc5f54d3bf9d714
morioka <morioka@1-10.com>
2016-08-13 16:10:36 +0900

    Carthageに対応。

---
+++ SimpleOSC/SimpleOSC.h
@@ -0,0 +1,19 @@
+//
+//  SimpleOSC.h
+//  SimpleOSC
+//
+//  Created by MORIOKAToyoshi on 2016/08/13.
+//  Copyright © 2016年 ___MORIOKAToyoshi___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleOSC.
+FOUNDATION_EXPORT double SimpleOSCVersionNumber;
+
+//! Project version string for SimpleOSC.
+FOUNDATION_EXPORT const unsigned char SimpleOSCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleOSC/PublicHeader.h>
+
+
