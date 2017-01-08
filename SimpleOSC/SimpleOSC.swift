e528d99bc436bf6187fdd925edf4868f57eefbed
morioka <morioka@1-10.com>
2017-01-08 18:03:19 +0900

    swift3に対応。

--- SimpleOSC/SimpleOSC.swift
+++ SimpleOSC/SimpleOSC.swift
@@ -10,6 +10,10 @@ import Foundation
 
 public struct SimpleOSC {
     
+    func ptr<T: Any>(_ p: UnsafeMutablePointer<T>) -> UnsafeMutablePointer<T> {
+        return p
+    }
+    
     var oscMessageArray:Array<SimpleOSCMessage>
     
     public init(){
@@ -20,36 +24,36 @@ public struct SimpleOSC {
         oscMessageArray.removeAll()
     }
     
-    public func getOSCData() -> NSData {
+    public func getOSCData() -> Data {
         let result = NSMutableData()
         let header = SimpleOSCMessage.paddingString("#bundle")
-        let timeTag = "\0\0\0\0\0\0\0\0" // this is timetag
-        result.appendData(header.dataUsingEncoding(NSUTF8StringEncoding)!)
-        result.appendData(timeTag.dataUsingEncoding(NSUTF8StringEncoding)!)
+        let timeTag = "\0\0\0\0\0\0\0\0" // this is timetag not support
+        result.append(header.data(using: String.Encoding.utf8)!)
+        result.append(timeTag.data(using: String.Encoding.utf8)!)
         
         if oscMessageArray.count != 0 {
             for i in 0...oscMessageArray.count-1 {
                 let message = oscMessageArray[i].getOSCData()
-                result.appendData(SimpleOSCMessage.uInt2Data(UInt32(message.length)))
-                result.appendData(message)
+                result.append(SimpleOSCMessage.uInt2Data(UInt32(message.count)) as Data)
+                result.append(message as Data)
             }
         }
 
-        return result
+        return result as Data
     }
     
-    public func parseOSCData(data:NSData) -> Dictionary<String, AnyObject> {
+    public func parseOSCData(_ data:Data) -> Dictionary<String, AnyObject> {
         var result:Dictionary<String, AnyObject> = [:]
         
         if hasBundle(data) {
             print("data has bundle.")
             
-            if data.length < 16 {
+            if data.count < 16 {
                 print("error. too short data.")
             }
             
             // remove bundle text and time code
-            result = parseOSCMessage(data.subdataWithRange(NSRange(16..<data.length)))
+            result = parseOSCMessage(data.subdata(in: Range(16..<data.count)) as Data)
             
         }else{
             print("data does not has bundle.")
@@ -59,55 +63,54 @@ public struct SimpleOSC {
         return result
     }
     
-    func hasBundle(data:NSData) -> Bool {
-        if data.length >= 8 {
-            let headString = NSString(data: data.subdataWithRange(NSRange(0..<7)), encoding:NSUTF8StringEncoding)
-            if headString!.isEqualToString("#bundle") {
+    func hasBundle(_ data:Data) -> Bool {
+        if data.count >= 8 {
+            let headString = NSString(data: data.subdata(in: Range(0..<7)), encoding:String.Encoding.utf8.rawValue)
+            if headString!.isEqual(to: "#bundle") {
                 return true;
             }
         }
         return false
     }
     
-    func parseOSCMessage(data:NSData) -> Dictionary<String, AnyObject> {
+    func parseOSCMessage(_ data:Data) -> Dictionary<String, AnyObject> {
         var resultArray:Dictionary<String, AnyObject> = [:]
         var head = 0
         
-        print("message array size:\(data.length)")
+        print("message array size:\(data.count)")
         
-        while head <= data.length {
+        while head <= data.count {
             print("header:\(head).")
             // read message size
-            let sizeData = data.subdataWithRange(NSRange(head..<head+4))
-            var size:Int = 0
-            sizeData.getBytes(&size, length: sizeof(Int32))
-            print("osc message size is \(size).")
+            let sizeData = data.subdata(in: Range(head..<head+4))
+            print("sizedata:\(sizeData.count)")
+            var size: Int32 = 0
+            size = sizeData.withUnsafeBytes{ $0.pointee }
+            print("osc message size is \(size.byteSwapped).")
             
             // get osc data
-            let oscData = data.subdataWithRange(NSRange(head+4..<head+4+size))
+            let oscData = data.subdata(in: Range(head + 4..<head + 4 + Int(size.byteSwapped)))
             
             // get address
-            let addressAndData = getAddress(oscData)
+            let addressAndData = getAddress(oscData as Data)
             let newAddress = addressAndData.address
-            //newAddress.stringByReplacingOccurrencesOfString("\"", withString: "")
-            //newAddress.stringByReplacingOccurrencesOfString("\0", withString: " ")
-            let address = newAddress.componentsSeparatedByString("\0")
+            let address = newAddress.components(separatedBy: "\0")
             // get data array
-            let oscDataArray = oscData.subdataWithRange(NSRange(addressAndData.startArgs..<oscData.length))
-            let resultDataArray = getDataArray(oscDataArray)
+            let oscDataArray = oscData.subdata(in: Range(addressAndData.startArgs..<oscData.count))
+            let resultDataArray = getDataArray(oscDataArray as Data)
             
-            resultArray[address[0]] = resultDataArray.resultArray
+            resultArray[address[0]] = resultDataArray.resultArray as AnyObject?
             
-            head = head + size + 4
+            head = head + Int(size.byteSwapped) + 4
             
-            if head >= data.length {
+            if head >= data.count {
                 break
             }
         }
         return resultArray
     }
     
-    func getDataArray(srcData:NSData) -> (success:Bool, resultArray:Array<AnyObject>) {
+    func getDataArray(_ srcData:Data) -> (success:Bool, resultArray:Array<AnyObject>) {
         var resultArray:Array<AnyObject> = []
         var argsTypeArray:Array<String> = []
         
@@ -116,12 +119,12 @@ public struct SimpleOSC {
         
         // make argsTypeArray
         while argsCheckFinish != true {
-            let firstArgsData = String(NSString(data: srcData.subdataWithRange(NSRange(head..<head+4)), encoding:NSUTF8StringEncoding)!)
+            let firstArgsData = String(String(data: srcData.subdata(in: Range(head..<head+4)), encoding:String.Encoding(rawValue: String.Encoding.utf8.rawValue))!)
             head = head + 4
             
             print("character:\(firstArgsData)")
             
-            for character in firstArgsData.characters{
+            for character in (firstArgsData?.characters)!{
                 print("\(character)")
                 if character == "i" {
                     argsTypeArray.append("int")
@@ -134,7 +137,7 @@ public struct SimpleOSC {
                     argsCheckFinish = true
                     break
                 }else{
-                    // athor args. sorry not support.
+                    // another args. sorry not support.
                 }
             }
         }
@@ -142,27 +145,31 @@ public struct SimpleOSC {
         // headから実際のvalueがある
         for arg in argsTypeArray {
             if arg == "int" {
-                let paramsData = srcData.subdataWithRange(NSRange(head..<head + 4))
-                var data:Int = 0
-                paramsData.getBytes(&data, length: sizeof(Int32))
-                //print("data:\(data)")
+                let paramsData = srcData.subdata(in: Range(head..<head + 4))
+                // 12/6
+                var tempData: Int32 = 0
+                tempData = paramsData.withUnsafeBytes{ $0.pointee }
+                let data = tempData.byteSwapped
                 head = head + 4
-                resultArray.append(data)
+                resultArray.append(data as AnyObject)
                 
             }else if arg == "float" {
-                let paramsData = srcData.subdataWithRange(NSRange(head..<head + 4))
-                var data:CFSwappedFloat32 = CFSwappedFloat32()
-                paramsData.getBytes(&data, length: sizeof(Float32))
-                //print("data:\(CFConvertFloatSwappedToHost(data))")
+                let paramsData = srcData.subdata(in: Range(head..<head + 4))
+
+                var data: CFSwappedFloat32 = CFSwappedFloat32()
+                let rawPtr = UnsafeMutableRawPointer(&data)
+                let opaquePtr = OpaquePointer(rawPtr)
+                let contextPtr = UnsafeMutablePointer<UInt8>(opaquePtr)
+                paramsData.copyBytes(to: contextPtr, count: MemoryLayout<Float32>.size)
                 head = head + 4
-                resultArray.append(CFConvertFloatSwappedToHost(data))
+                resultArray.append(CFConvertFloatSwappedToHost(data) as AnyObject)
                 
             }else if arg == "string" {
                 var startDoubleQuat = false
                 var endDoubleQuat = false
                 var resultString = ""
-                while !endDoubleQuat || head < srcData.length {
-                    let stringData = String(NSString(data: srcData.subdataWithRange(NSRange(head..<head+4)), encoding:NSUTF8StringEncoding)!)
+                while !endDoubleQuat || head < srcData.count {
+                    let stringData = String(NSString(data: srcData.subdata(in: Range(head..<head+4)), encoding:String.Encoding.utf8.rawValue)!)
                     head = head + 4
                     resultString += stringData
                     for character in stringData.characters{
@@ -172,10 +179,8 @@ public struct SimpleOSC {
                                 
                             }else{
                                 endDoubleQuat = true
-                                var newResultStringArray = resultString.componentsSeparatedByString("\0")
-                                resultArray.append(newResultStringArray[0])
-                                //print("string read finish! string:\(resultString)")
-                                //break
+                                var newResultStringArray = resultString.components(separatedBy:"\0")
+                                resultArray.append(newResultStringArray[0] as AnyObject)
                             }
                         }
                     }
@@ -196,18 +201,18 @@ public struct SimpleOSC {
         return (true, resultArray)
     }
     
-    func getAddress(srcData:NSData) -> (success:Bool, address:String, startArgs:Int) {
+    func getAddress(_ srcData:Data) -> (success:Bool, address:String, startArgs:Int) {
         let parser = getFirstComma(srcData)
-        return (parser.success, String(NSString(data: srcData.subdataWithRange(NSRange(0..<parser.point)), encoding:NSUTF8StringEncoding)!), parser.point)
+        return (parser.success, String(String(data: srcData.subdata(in: Range(0..<parser.point)), encoding:String.Encoding(rawValue: String.Encoding.utf8.rawValue))!), parser.point)
     }
     
-    func getFirstComma(data:NSData) -> (success:Bool, point:Int) {
+    func getFirstComma(_ data:Data) -> (success:Bool, point:Int) {
         var success = false
         var result = 0
         var head = 0;
         
-        while head + 2 < data.length || success != true {
-            let comma = NSString(data: data.subdataWithRange(NSRange(head..<head+1)), encoding:NSUTF8StringEncoding)
+        while head + 2 < data.count || success != true {
+            let comma = NSString(data: data.subdata(in: Range(head..<head+1)), encoding:String.Encoding.utf8.rawValue)
             
             if comma == "," {
                 result = head
@@ -220,7 +225,7 @@ public struct SimpleOSC {
         return (success, result)
     }
     
-    public mutating func addMessage(address:String, dataArray:Array<Any>){
+    public mutating func addMessage(_ address:String, dataArray:Array<Any>){
         var newMessage = SimpleOSCMessage()
         newMessage.setAddress("/\(address)")
         
@@ -248,21 +253,21 @@ public struct SimpleOSC {
         oscMessageArray.append(newMessage)
     }
     
-    public mutating func addMessage(address:String, data:Int32){
+    public mutating func addMessage(_ address:String, data:Int32){
         var newMessage = SimpleOSCMessage()
         newMessage.setAddress("/\(address)")
         newMessage.addInt32Param(data)
         oscMessageArray.append(newMessage)
     }
     
-    public mutating func addMessage(address:String, data:Float32){
+    public mutating func addMessage(_ address:String, data:Float32){
         var newMessage = SimpleOSCMessage()
         newMessage.setAddress("/\(address)")
         newMessage.addFloat32Param(data)
         oscMessageArray.append(newMessage)
     }
     
-    public mutating func addMessage(address:String, data:String){
+    public mutating func addMessage(_ address:String, data:String){
         var newMessage = SimpleOSCMessage()
         newMessage.setAddress("/\(address)")
         newMessage.addStringParam(data)
--- SimpleOSC/SimpleOSCMessage.swift
+++ SimpleOSC/SimpleOSCMessage.swift
@@ -17,70 +17,72 @@ struct SimpleOSCMessage {
         dataArray = []
     }
     
-    func getOSCData() -> NSData {
-        var data:NSMutableData = NSMutableData()
-        data = SimpleOSCMessage.paddingString(address).dataUsingEncoding(NSUTF8StringEncoding)!.mutableCopy() as! NSMutableData
-        data.appendData(SimpleOSCMessage.paddingString(args).dataUsingEncoding(NSUTF8StringEncoding)!)
+    func getOSCData() -> Data {
+        var data:Data = Data()
+        data = SimpleOSCMessage.paddingString(address).data(using: String.Encoding.utf8)! 
+        data.append(SimpleOSCMessage.paddingString(args).data(using: String.Encoding.utf8)!)
         if dataArray.count != 0 {
             for i in 0...dataArray.count-1 {
                 if dataArray[i] is Int32 {
-                    data.appendData(SimpleOSCMessage.int2Data(dataArray[i] as! Int32))
+                    data.append(SimpleOSCMessage.int2Data(dataArray[i] as! Int32) as Data)
                     
                 }else if dataArray[i] is Float32 {
-                    data.appendData(SimpleOSCMessage.float2Data(dataArray[i] as! Float32))
+                    data.append(SimpleOSCMessage.float2Data(dataArray[i] as! Float32) as Data)
                     
                 }else if dataArray[i] is String  {
-                    data.appendData(SimpleOSCMessage.string2Data(dataArray[i] as! String))
+                    data.append(SimpleOSCMessage.string2Data(dataArray[i] as! String) as Data)
                     
                 }else{
-                    return NSData()
+                    return Data()
                 }
             }
         }
         return data
     }
     
-    mutating func setAddress(address:String) {
+    mutating func setAddress(_ address:String) {
         self.address = address
     }
     
-    mutating func addInt32Param(param:Int32) {
+    mutating func addInt32Param(_ param:Int32) {
         args += "i"
         dataArray.append(param)
     }
     
-    mutating func addFloat32Param(param:Float32) {
+    mutating func addFloat32Param(_ param:Float32) {
         args += "f"
         dataArray.append(param)
     }
     
-    mutating func addStringParam(param:String) {
+    mutating func addStringParam(_ param:String) {
         args += "s"
         dataArray.append("\"\(param)\"")
     }
     
-    static func int2Data(val:Int32) -> NSData{
+    static func int2Data(_ val:Int32) -> Data{
+        //var returnVal = CFSwapInt32(UInt32(val))
+        var returnVal = Int32.init(bigEndian: val)
+        //var returnVal = val.bigEndian
+        return (Data(bytes: &returnVal, count: MemoryLayout<Int32>.size))
+    }
+    
+    static func uInt2Data(_ val:UInt32) -> Data{
 //        var returnVal = CFSwapInt32(UInt32(val))
-        var returnVal = val.bigEndian
-        return (NSData(bytes: &returnVal, length: sizeof(Int32)))
+        var returnVal = Int32.init(bigEndian: Int32(val))
+        return (Data(bytes: &returnVal, count: MemoryLayout<Int32>.size))
     }
     
-    static func uInt2Data(val:UInt32) -> NSData{
-        var returnVal = CFSwapInt32(UInt32(val))
-        return (NSData(bytes: &returnVal, length: sizeof(Int32)))
-    }
-    
-    static func float2Data(val:Float32) -> NSData{
+    static func float2Data(_ val:Float32) -> Data{
         var returnVal = CFConvertFloatHostToSwapped(val)
-        return (NSData(bytes: &returnVal, length: sizeof(Float32)))
+        return (Data(bytes: &returnVal, count: MemoryLayout<Float32>.size))
     }
     
-    static func string2Data(val:String) -> NSData{
+    static func string2Data(_ val:String) -> Data{
         let paddingVal = SimpleOSCMessage.paddingString(val)
-        return paddingVal.dataUsingEncoding(NSUTF8StringEncoding)!
+        return (paddingVal.data(using: String.Encoding.utf8)! as NSData) as Data
     }
     
-    static func paddingString(original:String) -> String {
+    static func paddingString(_ original:String) -> String {
         var result = original
         let size = 4 - result.characters.count % 4
         
